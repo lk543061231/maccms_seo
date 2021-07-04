c529efe457ab677fdbbca7a7ddb6dd683007a927
lk543061231 <543061231@qq.com>
2021-07-04 21:42:52 +0800

    11111

--- static/global-c.js
+++ static/global-c.js
@@ -1,13 +1,13 @@
-(function(){
-    layui.use(['form'], function () {
+(function() {
+    layui.use(['form'], function() {
         var form = layui.form
-        form.on('select(tabVersion)',function(data){
-            if(data.value=='v10'){
-                window.location.href="../v10.html"
-            }else if(data.value=='v8'){
-                window.location.href="../v8.html"
+        form.on('select(tabVersion)', function(data) {
+            if (data.value == 'v10') {
+                window.location.href = "www.maccms.net/v10.html"
+            } else if (data.value == 'v8') {
+                window.location.href = "www.maccms.net/v8.html"
             }
-      })
+        })
     });
-    
+
 })()
